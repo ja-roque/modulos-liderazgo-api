9633a484f2a27e0d5f12d35b4053d108b29c0fe2
Unknown <jarb95@gmail.com>
2018-01-03 14:29:54 -0600

    Virtual Environment Django2 & Python3.6
    Initial commit of venv  and  authentication apis

---
+++ env3/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jroque/sites/modulos-app/modulos-liderazgo-api/env3/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
