3fdbc666e185ea09b530b9d4fa4b3fa8041eed77
Unknown <jarb95@gmail.com>
2018-01-17 00:33:54 -0600

    create Mech to parse doc to array

--- liderazgo_api/liderazgo_api/urls.py
+++ liderazgo_api/liderazgo_api/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import include, url
 from django.urls import path
 
 from rest_framework_jwt.views import refresh_jwt_token, verify_jwt_token
-from .views import RestrictedView
+from .views import RestrictedView, getDoc
 
 
 urlpatterns = [
@@ -26,10 +26,13 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^', include('rest_auth.urls')),
     url(r'^registration/', include('rest_auth.registration.urls')),
+    
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    
     url(r'^refresh-token/', refresh_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
 
     url(r'^restricted/$', RestrictedView.as_view()),
+    url(r'^getdoc/$', getDoc.as_view()),
 ]
--- liderazgo_api/liderazgo_api/views.py
+++ liderazgo_api/liderazgo_api/views.py
@@ -4,15 +4,41 @@ from rest_framework.permissions import IsAuthenticated
 
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
+from bs4 import BeautifulSoup
+from django.http import JsonResponse
+from pathlib import Path
+
+
+import os
+import re
+
 
 class RestrictedView(APIView):
-    permission_classes = (IsAuthenticated, )
-    authentication_classes = (JSONWebTokenAuthentication, )
+	permission_classes = (IsAuthenticated, )
+	authentication_classes = (JSONWebTokenAuthentication, )
 
-    def get(self, request):
-        data = {
-            'id': request.user.id,
-            'username': request.user.username,
-            'token': str(request.auth)
-        }
-        return Response(data)
+	def get(self, request):
+		data = {
+		'id': request.user.id,
+		'username': request.user.username,
+		'token': str(request.auth)
+		}
+		return Response(data)
+
+class getDoc(APIView):
+	__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+	def get(self, request):
+
+		slideObject = []
+		contents = Path(os.path.join(self.__location__, "doc1.txt")).read_text()
+
+		slidelist = re.findall('<slide>(.*?)</slide>', contents.replace('\n', '').replace('\t', ''), flags= re.DOTALL)
+		
+		for slide in slidelist:
+			 
+			fraglist = re.findall('<frag>(.*?)</frag>', slide.replace('\n', '').replace('\t', ''), flags= re.DOTALL)				
+			slideObject.append(fraglist)
+			pass
+						
+		return JsonResponse(slideObject, safe=False)
