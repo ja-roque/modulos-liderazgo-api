23e41eeaef5eca0f4b4b59d063791ac854ecfcad
Unknown <jarb95@gmail.com>
2018-03-05 19:48:07 -0600

    Commit exam model update

--- liderazgo_api/exam/models.py
+++ liderazgo_api/exam/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Exam(models.Model):
-    examScore = models.IntegerField(default=0, help_text="Total score of the exam")
-    attempts = models.IntegerField(default=0, help_text="Total of exam trials")
-    
+	user		= models.ForeignKey(User, related_name='user_exam', on_delete=models.CASCADE)
+	examScore 	= models.IntegerField(default=0, help_text="Total score of the exam")
+	attempts 	= models.IntegerField(default=0, help_text="Total of exam trials")
